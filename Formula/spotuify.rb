897acc24ebe1c6f2f451c804879a18335d3e79f6
Bhekani Khumalo <bhekanik@gmail.com>
2026-05-21 07:52:54 +0100

    spotuify 0.1.1

---
+++ Formula/spotuify.rb
@@ -0,0 +1,34 @@
+class Spotuify < Formula
+  desc "Keyboard-native Spotify TUI, CLI, and daemon"
+  homepage "https://github.com/planetaryescape/spotuify"
+  version "0.1.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/planetaryescape/spotuify/releases/download/v#{version}/spotuify-v#{version}-macos-aarch64.tar.gz"
+      sha256 "bb0b11babf81a17fdf687b8c189b5cbe0703449523c9aa72d027268068bc4478"
+    else
+      url "https://github.com/planetaryescape/spotuify/releases/download/v#{version}/spotuify-v#{version}-macos-x86_64.tar.gz"
+      sha256 "eb127ac271438113552d29d60e83008a039b250a4cc07425dcaba94750df5d97"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/planetaryescape/spotuify/releases/download/v#{version}/spotuify-v#{version}-linux-x86_64.tar.gz"
+      sha256 "4aee66d99e59a2219eb1c2509d3dc3e40df56d49d980f3bb49e5417ddf92e634"
+    end
+  end
+
+  def install
+    bin.install "spotuify"
+    prefix.install "README.md"
+    prefix.install "install" if File.directory?("install")
+    prefix.install "recipes" if File.directory?("recipes")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/spotuify --version")
+  end
+end
